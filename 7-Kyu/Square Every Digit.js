fc5067beda73fa0ab493f2d4d748ccbc67b15a3d
Prince BUGINGO <princebugingolearn@gmail.com>
2025-03-10 16:16:35 +0200

    another kata

--- 7-Kyu/Square Every Digit.js
+++ 7-Kyu/Square Every Digit.js
@@ -1,3 +1,10 @@
 function squareDigits(num){
-    return 0;
+    let arr1 = num.toString().split("");
+    let arr2=[]; 
+    arr1.forEach(element => {
+        arr2.push(Math.pow(element,2)); 
+    });
+
+    return Number(arr2.join(""));
   }
+  console.log(squareDigits(3212));
